02c389115590b5bdbabd93df02d53d501c2e3da5
Alan Torres <temp_peri@hotmail.com>
2017-06-27 16:50:40 -0500

    mainModel

---
+++ src/shoppingcart.js
@@ -0,0 +1,35 @@
+﻿let cart = (function (options) {
+
+    let storeOptions = options;
+    let addToCart = (product) => {
+
+    }
+    let increaseCount = (productId, value = 0) => {
+
+    }
+    let removeFromCart = (id) => {
+
+    }
+    let getCartItem = (id) => {
+
+    }
+    let getCartItems = () => {
+
+    }
+    let getCheckOutModel = () => {
+
+    }
+    let setTaxForProduct = (productId, taxObject) => {
+
+    }
+    return {
+        addToCart,
+        removeFromCart,
+        getCartItem,
+        getCartItems,
+        getCheckOutModel,
+        setTaxForProduct,
+        increaseCount,
+
+    }
+});
